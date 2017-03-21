5431f12a748955bc24596cf47675be216aa1ccf9
javathought <pascal.abaziou@gmail.com>
2017-03-21 22:08:37 +0100

    feature(qualification-round:A): add Counting Sheep solution
    add solution
    add some tests

---
+++ src/main/scala/puzzle2016/CountSheep.scala
@@ -0,0 +1,59 @@
+package puzzle2016
+
+import java.io.{File, PrintWriter}
+
+import scala.io.Source
+
+/**a: Counting Sheep
+  * Qualification Round 2016 -
+  */
+object CountSheep extends App {
+
+  val path = new File(this.getClass.getClassLoader.getResource("sheep_small.txt").toURI).getPath
+  val pathRes = new File(this.getClass.getClassLoader.getResource("sheep_large_res.txt").toURI).getPath
+
+
+  /**
+    * add digits of `cur` in Set `d`, so the set grows
+    * @param cur current number to read the digits
+    * @param d a Set containing already cumulated digits
+    * @return s Set with new digits of cur
+    */
+  def digits (cur: Int, d: Set[Int]): Set[Int] = {
+    if (cur == 0) d else digits(cur/10, d + cur%10)
+  }
+
+  def countSheep(start: Int): Int = {
+
+    /*
+      Create an empty Set (so contains uniques) to cumulates digits in numbers until all digits are presents => size is 10
+     */
+
+    def helper (s:Int, d: Set[Int], n: Int): Int = {
+      val newDigits = digits(s*n, d)  // add digits of s*n in the set
+      if (newDigits.size == 10) s*n else helper(s, newDigits, n+1)
+    }
+
+    helper(start, Set(), 1) // starts with an empty Set
+
+  }
+
+  val pw = new PrintWriter(new File("res_small.txt" ))
+  var i = 0
+  for (line <- Source.fromFile(path).getLines()) {
+    if (i == 0) {
+      println(s"$line cases")
+    } else {
+      if (line.toInt == 0)
+        pw.write(s"Case #$i: INSOMNIA\n")
+      else {
+        val res = countSheep(line.toInt)
+        pw.write(s"Case #$i: $res\n")
+      }
+
+    }
+    i = i + 1
+  }
+  pw.close()
+
+}
---
+++ src/test/scala/puzzle2016/CountSheepTest.scala
@@ -0,0 +1,26 @@
+package puzzle2016
+
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+import puzzle2016.CountSheep._
+
+/**
+  * Some test on Count Sheep
+  */
+@RunWith(classOf[JUnitRunner])
+class CountSheepTest extends FunSuite {
+
+
+  test("Count Sheep is ok") {
+
+    println(digits(34, Set()))
+
+    assert(countSheep(1) === 10)
+    assert(countSheep(2) === 90)
+    assert(countSheep(11) === 110)
+    assert(countSheep(1692) === 5076)
+
+  }
+
+}
